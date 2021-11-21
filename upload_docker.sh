c823890f587a3ec10ab9ea70152c8125f9a6fdee
Gimmy <gamalgiali@gmail.com>
2021-11-21 14:38:18 -0500

    Push image to docker hub

--- upload_docker.sh
+++ upload_docker.sh
@@ -5,11 +5,15 @@
 
 # Step 1:
 # Create dockerpath
-# dockerpath=<your docker ID/path>
+dockerpath=gimmyali/api
 
 # Step 2:  
 # Authenticate & tag
 echo "Docker ID and Image: $dockerpath"
 
+
 # Step 3:
 # Push image to a docker repository
+username=gimmyali
+docker --login username $username
+docker push gimmyali/api
