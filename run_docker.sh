41281716a8653a69867b62245b46b6344f71ab5b
Gimmy <gamalgiali@gmail.com>
2021-11-20 21:43:56 -0500

    Operationalize an ML App

---
+++ run_docker.sh
@@ -0,0 +1,21 @@
+# !/usr/bin/env bash
+
+## Complete the following steps to get Docker running locally
+
+# Step 1:
+# Build image and add a descriptive tag
+docker build --tag=gimmyali/api .
+
+# Step 2:
+# List docker images
+docker image ls
+
+# Step 3:
+# Push Docker image 
+username=gimmyali
+docker --login username $username
+docker push gimmyali/api
+
+# Step 4:
+# Run flask app
+docker run -p 8080:80 api
